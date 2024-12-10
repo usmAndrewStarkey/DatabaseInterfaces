c394b134a9782868519c07952a346d28d700ba56
AndrewStarkey <andy@thestarkeys.org>
2024-12-09 19:52:45 -0600

    Add project files.

---
+++ DatabaseInterfaces/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DatabaseInterfaces
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ DatabaseInterfaces/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DatabaseInterfaces
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
